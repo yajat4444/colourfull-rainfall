7e42b8308df35b2c5f4ea1426300fcc934880ec1
yajat4444 <61344107+yajat4444@users.noreply.github.com>
2020-06-13 23:29:05 +0530

    Add files via upload

---
+++ P5/P5/sketch.js
@@ -0,0 +1,20 @@
+
+
+
+
+var drops = [];
+
+function setup() {
+  createCanvas(600, 400);
+  for (var i = 0; i < 500; i++) {
+    drops[i] = new Drop();
+  }
+}
+
+function draw() {
+  background(230, 230, 250);
+  for (var i = 0; i < drops.length; i++) {
+    drops[i].fall();
+    drops[i].show();
+  }
+}
